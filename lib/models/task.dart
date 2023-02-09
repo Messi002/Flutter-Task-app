2d5725f90fa2f14780502bb3f53bf75dec111912
Messi002 <rostandmessi2@gmail.com>
2023-02-09 03:56:53 +0100

    task app

---
+++ lib/models/task.dart
@@ -0,0 +1,48 @@
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class Task extends Equatable{
+  final String title;
+   bool? isDone;
+   bool? isDeleted;
+
+  Task({required this.title, this.isDone, this.isDeleted})
+  {
+    isDone = isDone ?? false;
+    isDeleted = isDeleted ?? false;
+  }
+
+  Task copyWith({
+    String? title,
+    bool? isDone,
+    bool? isDeleted,
+  }) {
+    return Task(
+      title: title ?? this.title,
+      isDone: isDone ?? this.isDone,
+      isDeleted: isDeleted ?? this.isDeleted,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'title': title,
+      'isDone': isDone,
+      'isDeleted': isDeleted,
+    };
+  }
+
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      title: json['title'] as String, //json['title'] ?? ''
+      isDone: json['isDone'] != null ? json['isDone'] as bool : null, //json['isDone']
+      isDeleted: json['isDeleted'] != null ? json['isDeleted'] as bool : null, //json['isDeleted']
+    );
+  }
+  
+  @override
+  List<Object?> get props =>[title, isDeleted, isDone];
+
+
+}
